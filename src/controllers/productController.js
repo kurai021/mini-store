c1e56ada98df5f17a6364a7e512e3569c3607e45
Richard Armuelles <alexanderarmuelles@riseup.net>
2023-07-17 21:41:16 -0500

    chore: Aplicar formateo y correcciones de linter en la funcionalidad de obtención de productos con paginación

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,24 +2,27 @@ const Product = require('../models/product');
 
 // Obtener productos con paginación
 async function getPaginatedProducts(req, res) {
-    try {
-        const { page = 1, limit = 10 } = req.query;
-        const offset = (page - 1) * limit;
-        const { count, rows: products } = await Product.findAndCountAll({ offset, limit });
-        const totalPages = Math.ceil(count / limit);
+  try {
+    const { page = 1, limit = 10 } = req.query;
+    const offset = (page - 1) * limit;
+    const { count, rows: products } = await Product.findAndCountAll({
+      offset,
+      limit,
+    });
+    const totalPages = Math.ceil(count / limit);
 
-        res.json({
-            totalProducts: count,
-            totalPages,
-            currentPage: page,
-            products,
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-    }
+    res.json({
+      totalProducts: count,
+      totalPages,
+      currentPage: page,
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
 }
 
 module.exports = {
-    getPaginatedProducts,
+  getPaginatedProducts,
 };
--- src/models/product.js
+++ src/models/product.js
@@ -1,30 +1,34 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Product = sequelize.define('Product', {
+const Product = sequelize.define(
+  'Product',
+  {
     name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     category: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     store: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
     },
     active: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
     },
-}, {
+  },
+  {
     tableName: 'products',
-    timestamps: false
-});
+    timestamps: false,
+  }
+);
 
 module.exports = Product;
